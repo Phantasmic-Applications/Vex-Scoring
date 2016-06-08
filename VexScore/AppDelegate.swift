5dec91f290bd6cb27d64048320e7790034c5d7b6
Pj Mara <funpjm@gmail.com>
2016-06-08 13:02:44 -0400

    hello pls

--- VexScore/AppDelegate.swift
+++ VexScore/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {//test hello pj
+class AppDelegate: UIResponder, UIApplicationDelegate {//test hello andrew
 
     var window: UIWindow?
 
