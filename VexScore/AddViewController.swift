7bd1841030f6664aabe439903bd40b322931976f
Andrew Turley <aturley123@gmail.com>
2016-06-09 11:32:10 -0400

    Added downloadCSV method

--- VexScore/AddViewController.swift
+++ VexScore/AddViewController.swift
@@ -19,6 +19,10 @@ class AddViewController: UIViewController {
     var teamauton: String = ""
     var robot: String = ""
     
+    var csv : CSV?
+    var csvString = ""
+    var masterTeams = [Team]()
+    
     
     var editingItem: NSManagedObject!
     
@@ -80,14 +84,12 @@ class AddViewController: UIViewController {
             
             
         } else {
-            
             let newitem = Model(entity: theEnt!, insertIntoManagedObjectContext: theContext)
             
             newitem.robot = TextFieldRobot.text!
             newitem.teamauton = TextFieldAuton.text!
             newitem.teamnum = TextFieldTeamNum.text!
-            
-            
+            downloadCSV()
         }
         
         do {
@@ -99,13 +101,56 @@ class AddViewController: UIViewController {
         }
         
         self.navigationController?.popViewControllerAnimated(true)
-        
-        
-        
     }
     
-    
-    
-    
-
+    func downloadCSV() {
+        let url = NSURL(string: "http://ajax.robotevents.com/tm/results/rankings/?format=csv&sku=RE-VRC-13-8611&div=1")
+        
+        if url != nil {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                if error == nil {
+                    
+                    let urlContent = NSString(data: data!, encoding: NSASCIIStringEncoding) as String!
+                    
+                    self.csvString = urlContent
+                    
+                    self.csvString = self.csvString.stringByReplacingOccurrencesOfString("\"", withString: "")
+                    
+                    self.csv = CSV(string: self.csvString)
+                    
+                    self.csv!.columns.popFirst()
+                    self.csv!.columns.popFirst()
+                    
+                    for index in 0..<self.csv!.rows.count {
+                        for data in 0..<self.csv!.rows[index].count {
+                            switch self.csv!.rows[index].first!.0 {
+                                case "rank":
+                                    self.csv!.rows[index].popFirst()
+                                case "ties":
+                                    self.csv!.rows[index].popFirst()
+                                case "wins":
+                                    self.csv!.rows[index].popFirst()
+                                case "losses":
+                                    self.csv!.rows[index].popFirst()
+                                case "teamname":
+                                    self.csv!.rows[index].popFirst()
+                                case "sp":
+                                    self.csv!.rows[index].popFirst()
+                                case "ap":
+                                    self.csv!.rows[index].popFirst()
+                                default:
+                                    self.csv!.rows[index].popFirst()
+                            }
+                        }
+                    }
+                }
+                else {
+                    print(error)
+                    self.csvString = "ERROR"
+                }
+            })
+            task.resume()
+        }
+    }
 }
